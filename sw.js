c10f7d872afedaae330f3ce58af00f2f3bf714d3
HaKunaMatata <sadbanana21378@gmail.com>
2025-12-10 16:08:08 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("jajko-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
